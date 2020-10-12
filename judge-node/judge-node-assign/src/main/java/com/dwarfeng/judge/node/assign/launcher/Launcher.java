c00e028ba619b9fc01837c291839f1154ede3917
DwArFeng <915724865@qq.com>
2020-10-12 11:23:44 +0800

    升级 spring-terminator 并优化启动器代码
    Signed-off-by: DwArFeng <915724865@qq.com>

--- judge-node/judge-node-assign/src/main/java/com/dwarfeng/judge/node/assign/launcher/Launcher.java
+++ judge-node/judge-node-assign/src/main/java/com/dwarfeng/judge/node/assign/launcher/Launcher.java
@@ -2,11 +2,10 @@ package com.dwarfeng.judge.node.assign.launcher;
 
 import com.dwarfeng.judge.node.assign.handler.LauncherSettingHandler;
 import com.dwarfeng.judge.stack.service.AssignQosService;
-import com.dwarfeng.springterminator.stack.handler.Terminator;
+import com.dwarfeng.springterminator.sdk.util.ApplicationUtil;
 import com.dwarfeng.subgrade.stack.exception.ServiceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * 程序启动器。
@@ -19,37 +18,35 @@ public class Launcher {
     private final static Logger LOGGER = LoggerFactory.getLogger(Launcher.class);
 
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(
+        ApplicationUtil.launch(new String[]{
                 "classpath:spring/application-context*.xml",
                 "file:opt/opt*.xml",
-                "file:optext/opt*.xml");
-        ctx.registerShutdownHook();
-        ctx.start();
-        LauncherSettingHandler launcherSettingHandler = ctx.getBean(LauncherSettingHandler.class);
-        // 判断是否开启指派服务。
-        long startAssignDelay = launcherSettingHandler.getStartAssignDelay();
-        AssignQosService assignQosService = ctx.getBean(AssignQosService.class);
-        if (startAssignDelay == 0) {
-            LOGGER.info("立即启动指派服务...");
-            try {
-                assignQosService.startAssign();
-            } catch (ServiceException e) {
-                LOGGER.error("无法启动指派服务，异常原因如下", e);
+                "file:optext/opt*.xml"
+        }, ctx -> {
+            LauncherSettingHandler launcherSettingHandler = ctx.getBean(LauncherSettingHandler.class);
+            // 判断是否开启指派服务。
+            long startAssignDelay = launcherSettingHandler.getStartAssignDelay();
+            AssignQosService assignQosService = ctx.getBean(AssignQosService.class);
+            if (startAssignDelay == 0) {
+                LOGGER.info("立即启动指派服务...");
+                try {
+                    assignQosService.startAssign();
+                } catch (ServiceException e) {
+                    LOGGER.error("无法启动指派服务，异常原因如下", e);
+                }
+            } else if (startAssignDelay > 0) {
+                LOGGER.info(startAssignDelay + " 毫秒后启动指派服务...");
+                try {
+                    Thread.sleep(startAssignDelay);
+                } catch (InterruptedException ignored) {
+                }
+                LOGGER.info("启动指派服务...");
+                try {
+                    assignQosService.startAssign();
+                } catch (ServiceException e) {
+                    LOGGER.error("无法启动指派服务，异常原因如下", e);
+                }
             }
-        } else if (startAssignDelay > 0) {
-            LOGGER.info(startAssignDelay + " 毫秒后启动指派服务...");
-            try {
-                Thread.sleep(startAssignDelay);
-            } catch (InterruptedException ignored) {
-            }
-            LOGGER.info("启动指派服务...");
-            try {
-                assignQosService.startAssign();
-            } catch (ServiceException e) {
-                LOGGER.error("无法启动指派服务，异常原因如下", e);
-            }
-        }
-        Terminator terminator = ctx.getBean(Terminator.class);
-        System.exit(terminator.getExitCode());
+        });
     }
 }
--- judge-node/judge-node-evaluate/src/main/java/com/dwarfeng/judge/node/evaluate/launcher/Launcher.java
+++ judge-node/judge-node-evaluate/src/main/java/com/dwarfeng/judge/node/evaluate/launcher/Launcher.java
@@ -2,11 +2,10 @@ package com.dwarfeng.judge.node.evaluate.launcher;
 
 import com.dwarfeng.judge.node.evaluate.handler.LauncherSettingHandler;
 import com.dwarfeng.judge.stack.service.EvaluateQosService;
-import com.dwarfeng.springterminator.stack.handler.Terminator;
+import com.dwarfeng.springterminator.sdk.util.ApplicationUtil;
 import com.dwarfeng.subgrade.stack.exception.ServiceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * 程序启动器。
@@ -19,37 +18,35 @@ public class Launcher {
     private final static Logger LOGGER = LoggerFactory.getLogger(Launcher.class);
 
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(
+        ApplicationUtil.launch(new String[]{
                 "classpath:spring/application-context*.xml",
                 "file:opt/opt*.xml",
-                "file:optext/opt*.xml");
-        ctx.registerShutdownHook();
-        ctx.start();
-        LauncherSettingHandler launcherSettingHandler = ctx.getBean(LauncherSettingHandler.class);
-        // 判断是否开启评估服务。
-        long startEvaluateDelay = launcherSettingHandler.getStartEvaluateDelay();
-        EvaluateQosService evaluateQosService = ctx.getBean(EvaluateQosService.class);
-        if (startEvaluateDelay == 0) {
-            LOGGER.info("立即启动评估服务...");
-            try {
-                evaluateQosService.startEvaluate();
-            } catch (ServiceException e) {
-                LOGGER.error("无法启动评估服务，异常原因如下", e);
+                "file:optext/opt*.xml"
+        }, ctx -> {
+            LauncherSettingHandler launcherSettingHandler = ctx.getBean(LauncherSettingHandler.class);
+            // 判断是否开启评估服务。
+            long startEvaluateDelay = launcherSettingHandler.getStartEvaluateDelay();
+            EvaluateQosService evaluateQosService = ctx.getBean(EvaluateQosService.class);
+            if (startEvaluateDelay == 0) {
+                LOGGER.info("立即启动评估服务...");
+                try {
+                    evaluateQosService.startEvaluate();
+                } catch (ServiceException e) {
+                    LOGGER.error("无法启动评估服务，异常原因如下", e);
+                }
+            } else if (startEvaluateDelay > 0) {
+                LOGGER.info(startEvaluateDelay + " 毫秒后启动评估服务...");
+                try {
+                    Thread.sleep(startEvaluateDelay);
+                } catch (InterruptedException ignored) {
+                }
+                LOGGER.info("启动评估服务...");
+                try {
+                    evaluateQosService.startEvaluate();
+                } catch (ServiceException e) {
+                    LOGGER.error("无法启动评估服务，异常原因如下", e);
+                }
             }
-        } else if (startEvaluateDelay > 0) {
-            LOGGER.info(startEvaluateDelay + " 毫秒后启动评估服务...");
-            try {
-                Thread.sleep(startEvaluateDelay);
-            } catch (InterruptedException ignored) {
-            }
-            LOGGER.info("启动评估服务...");
-            try {
-                evaluateQosService.startEvaluate();
-            } catch (ServiceException e) {
-                LOGGER.error("无法启动评估服务，异常原因如下", e);
-            }
-        }
-        Terminator terminator = ctx.getBean(Terminator.class);
-        System.exit(terminator.getExitCode());
+        });
     }
 }
