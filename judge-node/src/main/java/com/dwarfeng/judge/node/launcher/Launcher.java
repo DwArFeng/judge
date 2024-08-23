f6ee25bf0cd39a91f703cea9f328248fef60a8c2
DwArFeng <915724865@qq.com>
2024-08-23 10:13:20 +0800

    启动器优化
    将入口方法中完整独立的功能封装在子方法中，使入口方法代码结构更加清晰
    
    Signed-off-by: DwArFeng <915724865@qq.com>

--- judge-node/src/main/java/com/dwarfeng/judge/node/launcher/Launcher.java
+++ judge-node/src/main/java/com/dwarfeng/judge/node/launcher/Launcher.java
@@ -8,6 +8,10 @@ import com.dwarfeng.springterminator.sdk.util.ApplicationUtil;
 import com.dwarfeng.subgrade.stack.exception.ServiceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+import java.util.Date;
 
 /**
  * 程序启动器。
@@ -25,49 +29,81 @@ public class Launcher {
                 "file:opt/opt*.xml",
                 "file:optext/opt*.xml"
         }, ctx -> {
-            LauncherSettingHandler launcherSettingHandler = ctx.getBean(LauncherSettingHandler.class);
-            //判断是否重置判断器和驱动器。
-            if (launcherSettingHandler.isResetDriverSupport()) {
-                LOGGER.info("重置驱动器支持...");
-                DriverSupportMaintainService maintainService = ctx.getBean(DriverSupportMaintainService.class);
-                try {
-                    maintainService.reset();
-                } catch (ServiceException e) {
-                    LOGGER.warn("驱动器支持重置失败，异常信息如下", e);
-                }
-            }
-            if (launcherSettingHandler.isResetJudgerSupport()) {
-                LOGGER.info("重置判断器支持...");
-                JudgerSupportMaintainService maintainService = ctx.getBean(JudgerSupportMaintainService.class);
-                try {
-                    maintainService.reset();
-                } catch (ServiceException e) {
-                    LOGGER.warn("判断器支持重置失败，异常信息如下", e);
-                }
-            }
-            // 判断是否开启判断服务。
-            long startJudgeDelay = launcherSettingHandler.getStartJudgeDelay();
-            JudgeQosService judgeQosService = ctx.getBean(JudgeQosService.class);
-            if (startJudgeDelay == 0) {
-                LOGGER.info("立即启动判断服务...");
-                try {
-                    judgeQosService.start();
-                } catch (ServiceException e) {
-                    LOGGER.error("无法启动判断服务，异常原因如下", e);
-                }
-            } else if (startJudgeDelay > 0) {
-                LOGGER.info(startJudgeDelay + " 毫秒后启动判断服务...");
-                try {
-                    Thread.sleep(startJudgeDelay);
-                } catch (InterruptedException ignored) {
-                }
-                LOGGER.info("启动判断服务...");
-                try {
-                    judgeQosService.start();
-                } catch (ServiceException e) {
-                    LOGGER.error("无法启动判断服务，异常原因如下", e);
-                }
-            }
+            // 根据启动器设置处理器的设置，选择性重置驱动器。
+            mayResetDriver(ctx);
+
+            // 根据启动器设置处理器的设置，选择性重置判断器。
+            mayResetJudger(ctx);
+
+            // 根据启动器设置处理器的设置，选择性启动判断服务。
+            mayStartJudge(ctx);
         });
+    }
+
+    private static void mayResetDriver(ApplicationContext ctx) {
+        // 获取启动器设置处理器，用于获取启动器设置，并按照设置选择性执行功能。
+        LauncherSettingHandler launcherSettingHandler = ctx.getBean(LauncherSettingHandler.class);
+
+        // 判断是否重置驱动器支持，并按条件执行重置操作。
+        if (launcherSettingHandler.isResetDriverSupport()) {
+            LOGGER.info("重置驱动器支持...");
+            DriverSupportMaintainService maintainService = ctx.getBean(DriverSupportMaintainService.class);
+            try {
+                maintainService.reset();
+            } catch (ServiceException e) {
+                LOGGER.warn("驱动器支持重置失败，异常信息如下", e);
+            }
+        }
+    }
+
+    private static void mayResetJudger(ApplicationContext ctx) {
+        // 获取启动器设置处理器，用于获取启动器设置，并按照设置选择性执行功能。
+        LauncherSettingHandler launcherSettingHandler = ctx.getBean(LauncherSettingHandler.class);
+
+        // 判断是否重置判断器支持，并按条件执行重置操作。
+        if (launcherSettingHandler.isResetJudgerSupport()) {
+            LOGGER.info("重置判断器支持...");
+            JudgerSupportMaintainService maintainService = ctx.getBean(JudgerSupportMaintainService.class);
+            try {
+                maintainService.reset();
+            } catch (ServiceException e) {
+                LOGGER.warn("判断器支持重置失败，异常信息如下", e);
+            }
+        }
+    }
+
+    private static void mayStartJudge(ApplicationContext ctx) {
+        // 获取启动器设置处理器，用于获取启动器设置，并按照设置选择性执行功能。
+        LauncherSettingHandler launcherSettingHandler = ctx.getBean(LauncherSettingHandler.class);
+
+        // 获取程序中的 ThreadPoolTaskScheduler，用于处理计划任务。
+        ThreadPoolTaskScheduler scheduler = ctx.getBean(ThreadPoolTaskScheduler.class);
+
+        // 获取判断 QOS 服务。
+        JudgeQosService judgeQosService = ctx.getBean(JudgeQosService.class);
+
+        // 判断判断处理器是否启动判断服务，并按条件执行不同的操作。
+        long startJudgeDelay = launcherSettingHandler.getStartJudgeDelay();
+        if (startJudgeDelay == 0) {
+            LOGGER.info("立即启动判断服务...");
+            try {
+                judgeQosService.start();
+            } catch (ServiceException e) {
+                LOGGER.error("无法启动判断服务，异常原因如下", e);
+            }
+        } else if (startJudgeDelay > 0) {
+            LOGGER.info("{} 毫秒后启动判断服务...", startJudgeDelay);
+            scheduler.schedule(
+                    () -> {
+                        LOGGER.info("启动判断服务...");
+                        try {
+                            judgeQosService.start();
+                        } catch (ServiceException e) {
+                            LOGGER.error("无法启动判断服务，异常原因如下", e);
+                        }
+                    },
+                    new Date(System.currentTimeMillis() + startJudgeDelay)
+            );
+        }
     }
 }
